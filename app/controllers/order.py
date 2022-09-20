53051be6a9b6bd982c0f1bf6b3aea50622705936
Abigail Cabascango <51092317+abigailscl@users.noreply.github.com>
2022-09-20 11:09:19 -0500

    fix: Bonus Fix order price calculation (#3)
    Co-authored-by: Abigail Cabascango <sacabascango1espe.edu.ec>

--- app/controllers/order.py
+++ app/controllers/order.py
@@ -8,12 +8,13 @@ from .base import BaseController
 
 class OrderController(BaseController):
     manager = OrderManager
+    NUMBER_DIGITS = 2
     __required_info = ('client_name', 'client_dni', 'client_address', 'client_phone', 'size_id')
 
     @staticmethod
     def calculate_order_price(size_price: float, ingredients: list):
-        price = sum(ingredient.price for ingredient in ingredients)
-        return round(price, 2)
+        price = size_price + sum(ingredient.price for ingredient in ingredients)
+        return round(price, OrderController.NUMBER_DIGITS)
 
     @classmethod
     def create(cls, order: dict):
