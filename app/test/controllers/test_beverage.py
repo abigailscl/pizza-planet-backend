f36f4493c8d64a771089ccbc21c8d460ca050e9b
Abigail Cabascango <sacabascango1espe.edu.ec>
2022-09-21 08:52:03 -0500

    test: Add test for controller beverage

---
+++ app/test/controllers/test_beverage.py
@@ -0,0 +1,17 @@
+import pytest
+
+def test_create(app, beverage: dict):
+    pass
+
+
+
+def test_update(app, beverage: dict):
+    pass
+
+
+def test_get_by_id(app, beverage: dict):
+    pass
+
+
+def test_get_all(app, beverage: list):
+    pass
