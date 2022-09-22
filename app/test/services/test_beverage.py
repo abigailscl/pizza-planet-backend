355cf46ad329e227579ac143bb5795594afc1de5
Abigail Cabascango <sacabascango1espe.edu.ec>
2022-09-22 00:21:44 -0500

    feat: Create beverage service

--- app/test/services/test_beverage.py
+++ app/test/services/test_beverage.py
@@ -13,7 +13,8 @@ def test_create_ingredient_service(create_beverage):
 
 def test_update_beverage_service(client, create_beverage, beverage_uri):
     current_beverage = create_beverage.json
-    update_data = {**current_beverage, 'name': get_random_string(), 'price': get_random_price(1, 5)}
+    update_data = {**current_beverage, 'name': get_random_string(),
+                   'price': get_random_price(1, 5)}
     response = client.put(beverage_uri, json=update_data)
     pytest.assume(response.status.startswith('200'))
     updated_beverage = response.json
@@ -33,6 +34,7 @@ def test_get_beverage_by_id_service(client, create_beverage, beverage_uri):
 def test_get_beverages_service(client, create_beverages, beverage_uri):
     response = client.get(beverage_uri)
     pytest.assume(response.status.startswith('200'))
-    returned_beverages = {beverage['_id']: beverage for beverage in response.json}
+    returned_beverages = {beverage['_id']: beverage
+                          for beverage in response.json}
     for beverage in create_beverages:
         pytest.assume(beverage['_id'] in returned_beverages)
