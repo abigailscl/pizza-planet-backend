a4f882d1e3b36d8ea1ad44556e52ea8604de8c32
Abigail Cabascango <sacabascango1espe.edu.ec>
2022-09-23 20:35:50 -0500

    refactor: parameterize upper bound in function and test get random names

--- app/test/utils/test_fake_data.py
+++ app/test/utils/test_fake_data.py
@@ -2,5 +2,6 @@ import pytest
 from app.utils.fake_data import get_random_names
 
 def test_get_random_names():
-    names = get_random_names()
-    assert(len(names) == 100)
+    UPPER_BOUNDER = 100
+    names = get_random_names(UPPER_BOUNDER)
+    assert(len(names) == UPPER_BOUNDER)
--- app/utils/fake_data.py
+++ app/utils/fake_data.py
@@ -1,8 +1,8 @@
 from faker import Faker
 
-def get_random_names():
+def get_random_names(upper_bpund):
     name = []
     fake = Faker('it_IT')
-    for _ in range(100):
+    for _ in range(upper_bpund):
         name.append(fake.name())
     return name
