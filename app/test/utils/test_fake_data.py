11b0ca22de75b2a70007af2e974ae6aed0000e30
Abigail Cabascango <sacabascango1espe.edu.ec>
2022-09-23 22:19:25 -0500

    test: create test get random list DNI

--- app/test/utils/test_fake_data.py
+++ app/test/utils/test_fake_data.py
@@ -4,6 +4,12 @@ from app.utils.fake_data import get_random_names
 
 def test_get_random_names():
     LANGUAGE_NAME = 'it_IT'
-    UPPER_BOUNDER = 100
-    names = get_random_names(LANGUAGE_NAME, UPPER_BOUNDER)
-    assert(len(names) is UPPER_BOUNDER)
+    NUMBER_NAMES = 100
+    names = get_random_names(LANGUAGE_NAME, NUMBER_NAMES)
+    assert(len(names) is NUMBER_NAMES)
+
+
+def test_get_random_list_dni():
+    NUMBER_DNI = 100
+    list_dni = get_random_list_dni(NUMBER_DNI)
+    assert(len(list_dni) is NUMBER_DNI)
