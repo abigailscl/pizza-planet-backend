85f091112ab3c3dbe7fb481dad07ffb6d4654211
Abigail Cabascango <sacabascango1espe.edu.ec>
2022-09-25 18:55:41 -0500

    refactor: refactor seeder, split logic into functions

--- app/seeders/seeder.py
+++ app/seeders/seeder.py
@@ -1,8 +1,8 @@
 from flask_seeder import Seeder, Faker
 from flask_seeder.generator import Sequence, Integer
 
-from app.repositories.models import (Beverage, BeverageDetail, Size, Ingredient,
-                                     Order, OrderDetail)
+from app.repositories.models import (Beverage, BeverageDetail, Size,
+                                     Ingredient, Order, OrderDetail)
 from app.test.utils.functions_seeders import (get_random_ingredients,
                                               get_random_beverages,
                                               get_random_sizes)
@@ -52,7 +52,7 @@ class DBSeeder(Seeder):
         return faker_order
 
     def create_ingredient(self):
-        faker_order_details = Faker(
+        faker_order_ingredients = Faker(
             cls=OrderDetail,
             init={
                 '_id': Sequence(end=UPPER_BOUND_ORDERS),
@@ -61,13 +61,13 @@ class DBSeeder(Seeder):
                 'ingredient_id': Integer(end=len(FAKE_INGREDIENTS))
             }
         )
-        return faker_order_details
+        return faker_order_ingredients
 
     def create_beverages(self):
         faker_order_beverages = Faker(
             cls=BeverageDetail,
             init={
-                                '_id': Sequence(end=UPPER_BOUND_ORDERS),
+                '_id': Sequence(end=UPPER_BOUND_ORDERS),
                 'beverage_price': 0,
                 'order_id': Integer(end=UPPER_BOUND_ORDERS),
                 'beverage_id': Integer(end=len(FAKE_BEVERAGES))
@@ -75,57 +75,73 @@ class DBSeeder(Seeder):
             )
         return faker_order_beverages
 
+    def calculate_price_elements(self, order,
+                                 order_ingredients, order_beverages):
 
-    def calculate_order_price(self, orders, clients, order_details, order_beverages):
-        
+        total_price_ingredients = (
+            sum([ingredient.ingredient_price
+                for ingredient in order_ingredients
+                if order._id == ingredient.order_id]))
+        total_price_beverages = (
+                sum([beverage.beverage_price
+                    for beverage in order_beverages
+                    if order._id == beverage.order_id]))
+
+        return total_price_ingredients, total_price_beverages
+
+    def calculate_order_price(self, orders, clients,
+                              order_ingredients, order_beverages):
+
         for order in orders:
-            total_price_ingredients = (
-                sum([order_detail.ingredient_price
-                     for order_detail in order_details
-                     if order._id == order_detail.order_id]))
-            total_price_beverages = (
-                sum([order_beverage.beverage_price
-                     for order_beverage in order_beverages
-                     if order._id == order_beverage.order_id]))
+            total_price_ingredients, total_price_beverages = (
+                self.calculate_price_elements(
+                    order, order_ingredients, order_beverages))
             order.total_price = round(
                 FAKE_SIZES[order.size_id - 1]['price'] +
                 total_price_ingredients + total_price_beverages, NUMBER_DIGITS)
-            if not order.client_dni in clients:
+            if order.client_dni not in clients:
                 clients[order.client_dni] = {
                     'name': order.client_name,
                     'address': order.client_address,
                     'phone': order.client_phone
                 }
-                continue
-
             client = clients[order.client_dni]
             order.client_name = client['name']
             order.client_address = client['address']
             order.client_phone = client['phone']
         return orders
 
+    def set_order_detail(self, order_ingredients, order_beverages):
+        for order_ingredient, order_beverage in zip(order_ingredients,
+                                                    order_beverages):
+            order_ingredient.ingredient_price = (
+                FAKE_INGREDIENTS[order_ingredient.ingredient_id - 1]['price'])
+            order_beverage.beverage_price = (
+                FAKE_INGREDIENTS[order_beverage.beverage_id - 1]['price'])
+        return order_ingredients, order_beverages
+
     def create_orders(self):
         order_ingredients = self.create_ingredient().create(UPPER_BOUND_ORDERS)
         order_beverages = self.create_beverages().create(UPPER_BOUND_ORDERS)
-        for order_detail, order_beverage in zip(order_ingredients, order_beverages):
-            order_detail.ingredient_price = FAKE_INGREDIENTS[order_detail.ingredient_id - 1]['price']
-            order_beverage.beverage_price = FAKE_INGREDIENTS[order_beverage.beverage_id - 1]['price']
+        new_order_ingredients, new_order_beverages = self.set_order_detail(
+            order_ingredients, order_beverages)
 
         clients = dict()
         orders = self.create_order().create(UPPER_BOUND_ORDERS)
-        new_orders = self.calculate_order_price(orders, clients, order_ingredients, order_beverages)
+        new_orders = self.calculate_order_price(
+            orders, clients, new_order_ingredients, new_order_beverages)
         return new_orders, order_ingredients, order_beverages
 
     def run(self):
         sizes = self.create_order_elements(Size, FAKE_SIZES)
         ingredients = self.create_order_elements(Ingredient, FAKE_INGREDIENTS)
         beverages = self.create_order_elements(Beverage, FAKE_BEVERAGES)
-        orders, order_details, order_beverages = self.create_orders()
+        orders, order_ingredients, order_beverages = self.create_orders()
 
         self.db.session.add_all(sizes)
         self.db.session.add_all(ingredients)
         self.db.session.add_all(beverages)
         self.db.session.add_all(orders)
-        self.db.session.add_all(order_details)
+        self.db.session.add_all(order_ingredients)
         self.db.session.add_all(order_beverages)
         self.db.session.commit()
