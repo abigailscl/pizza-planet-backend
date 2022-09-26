94eba7ccd3e574f318bbfecf3c4370169cb756a4
Abigail Cabascango <sacabascango1espe.edu.ec>
2022-09-26 02:19:06 -0500

    refactor: refactor test, change names of test

--- app/test/repositories/reports/test_generate_reports.py
+++ app/test/repositories/reports/test_generate_reports.py
@@ -4,25 +4,25 @@ from app import flask_app
 from app.repositories.reports.report import MonthReport, CustomerReport
 from app.repositories.models import Ingredient, Order, OrderDetail, db
 
-def test_generate_report_better_month_revenue():
+def test_get_better_month_revenue():
     with flask_app.app_context():
         db.create_all()
         report = MonthReport(Order, db.session)
-        order = report.get_better_month_revenue()
-        pytest.assume(order is not None)
-        pytest.assume(order['month'] is not None)
-        pytest.assume(order['sale_amount'] is not None)
+        best_month = report.get_better_month_revenue()
+        pytest.assume(best_month is not None)
+        pytest.assume(best_month['month'] is not None)
+        pytest.assume(best_month['sale_amount'] is not None)
 
 
-def test_generate_report_best_customers():
+def test_get_best_customers():
     NUMBER_BEST_CUSTOMERS = 3
     with flask_app.app_context():
         db.create_all()
         report = CustomerReport(Order, OrderDetail, db.session)
-        curtomer = report.get_best_customers()
-        pytest.assume(curtomer is not None)
-        pytest.assume(len(curtomer) is NUMBER_BEST_CUSTOMERS)
+        best_curtomers = report.get_best_customers()
+        pytest.assume(best_curtomers is not None)
+        pytest.assume(len(best_curtomers) is NUMBER_BEST_CUSTOMERS)
 
 
-def generate_report_most_requested_ingredient():
+def test_get_best_seller_ingredient():
     pass
