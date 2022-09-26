962019cdd191f1915484090aa98ebd5d6aae0080
Abigail Cabascango <sacabascango1espe.edu.ec>
2022-09-26 11:01:24 -0500

    fix: errors in test report for service

--- app/test/services/test_report.py
+++ app/test/services/test_report.py
@@ -2,15 +2,15 @@
 import pytest
 
 
-def test_generate_report_better_month_revenue(client, report_uri, create_orders):
-    response = client.get(report_uri)
+def test_generate_report_better_month_revenue(client, report_month_uri):
+    response = client.get(report_month_uri)
     pytest.assume(response.status.startswith('200'))
     pytest.assume(response.json['month'])
     pytest.assume(response.json['sale_amount'])
 
 
-def test_generate_report_best_customers(client, report_uri):
-    response = client.get(report_uri)
+def test_generate_report_best_customers(client, report_customers_uri):
+    response = client.get(report_customers_uri)
     pytest.assume(response.status.startswith('200'))
     customers = response.jason
     for customer in customers:
@@ -19,8 +19,8 @@ def test_generate_report_best_customers(client, report_uri):
         pytest.assume(customer['dni'])
 
 
-def test_generate_report_best_seller_ingredient(client, report_uri):
-    response = client.get(report_uri)
+def test_generate_report_best_seller_ingredient(client, report_ingredients_uri):
+    response = client.get(report_ingredients_uri)
     pytest.assume(response.status.startswith('200'))
     pytest.assume(response['name'])
     pytest.assume(response['count'])
