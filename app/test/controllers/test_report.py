58821467d0eefe6cf75b2215a380c025116d6fbe
Abigail Cabascango <sacabascango1espe.edu.ec>
2022-09-26 10:04:50 -0500

    refactor: refactor test of controller reports

--- app/test/controllers/test_report.py
+++ app/test/controllers/test_report.py
@@ -1,29 +1,33 @@
 import pytest
 from app import flask_app
 
-from app.controllers.report import ReportController
-from app.repositories.models import Ingredient, Order, OrderDetail, db
+from app.controllers.report import *
 
 
-def test_get_report_better_month_revenue():
-    report, error = ReportController.generate_report_better_month_revenue()
+def test_generate_report_better_month_revenue():
     with flask_app.app_context():
         db.create_all()
-        pytest.assume(error is None)
-        pytest.assume(report['customers'] is not None)
-        pytest.assume(report['ingredient'] is not None)
-        pytest.assume(report['month'] is not None)
+        report = ReportController()
+        best_month = report.generate_report_better_month_revenue()
+        pytest.assume(best_month is not None)
+        pytest.assume(best_month['month'] is not None)
+        pytest.assume(best_month['sale_amount'] is not None)
 
 
-def test_get_report_best_customers():
+def test_generate_report_best_customers():
     NUMBER_BEST_CUSTOMERS = 3
-    report, error = ReportController.generate_report_best_customers()
-    pytest.assume(error is None)
-    pytest.assume(report is not None)
-    pytest.assume(len(report) is NUMBER_BEST_CUSTOMERS)
+    with flask_app.app_context():
+        db.create_all()
+        report = ReportController()
+        best_curtomers = report.generate_report_best_customers()
+        pytest.assume(best_curtomers is not None)
+        pytest.assume(len(best_curtomers) is NUMBER_BEST_CUSTOMERS)
 
 
 def test_generate_report_best_seller_ingredient():
-    report, error = ReportController.generate_report_best_seller_ingredient()
-    pytest.assume(report['name'] is not None)
-    pytest.assume(report['count'] > 1)
+    with flask_app.app_context():
+        db.create_all()
+        report = ReportController()
+        best_ingredient = report.generate_report_best_seller_ingredient()
+        pytest.assume(best_ingredient['name'] is not None)
+        pytest.assume(best_ingredient['count'] > 1)
