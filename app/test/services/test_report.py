4aa088e1c069f143e06ca6d53da7af34f3c16f20
Abigail Cabascango <sacabascango1espe.edu.ec>
2022-09-26 10:16:27 -0500

    test: create test for report service

--- app/test/services/test_report.py
+++ app/test/services/test_report.py
@@ -1,32 +1,26 @@
+
 import pytest
-from app import flask_app
-
-from app.controllers.report import *
-
-def test_get_better_month_revenue():
-    with flask_app.app_context():
-        db.create_all()
-        report = ReportController()
-        best_month = report.get_better_month_revenue()
-        pytest.assume(best_month is not None)
-        pytest.assume(best_month['month'] is not None)
-        pytest.assume(best_month['sale_amount'] is not None)
 
 
-def test_get_best_customers():
-    NUMBER_BEST_CUSTOMERS = 3
-    with flask_app.app_context():
-        db.create_all()
-        report = CustomerReport(Order, OrderDetail, db.session)
-        best_curtomers = report.get_best_customers()
-        pytest.assume(best_curtomers is not None)
-        pytest.assume(len(best_curtomers) is NUMBER_BEST_CUSTOMERS)
+def test_generate_report_better_month_revenue(client, report_uri, create_orders):
+    response = client.get(report_uri)
+    pytest.assume(response.status.startswith('200'))
+    pytest.assume(response.json['month'])
+    pytest.assume(response.json['sale_amount'])
 
 
-def test_get_best_seller_ingredient():
-    with flask_app.app_context():
-        db.create_all()
-        report = IngredientsReport(Order, OrderDetail, Ingredient, db.session)
-        best_ingredient = report.get_best_seller_ingredient()
-        pytest.assume(best_ingredient['name'] is not None)
-        pytest.assume(best_ingredient['count'] > 1)
+def test_generate_report_best_customers(client, report_uri):
+    response = client.get(report_uri)
+    pytest.assume(response.status.startswith('200'))
+    customers = response.jason
+    for customer in customers:
+        pytest.assume(customer['posicion'])
+        pytest.assume(customer['name'])
+        pytest.assume(customer['dni'])
+
+
+def test_generate_report_best_seller_ingredient(client, report_uri):
+    response = client.get(report_uri)
+    pytest.assume(response.status.startswith('200'))
+    pytest.assume(response['name'])
+    pytest.assume(response['count'])
